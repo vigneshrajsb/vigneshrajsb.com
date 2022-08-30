aa8e8929a2235174856e2ea6a3be9d02841c4e91
Vigneshraj Sekar Babu <vignesh.raj47@gmail.com>
2022-08-29 21:16:45 -0700

    fix: replace logo with text for now

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,12 +14,13 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Image
+        {/* <Image
           src="/../public/assets/logo.png" // TODO
           alt="logo"
           width="125"
           height="50"
-        />
+        /> */}
+        <p className="ml-5 text-base uppercase">vignesh</p>
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
@@ -57,12 +58,13 @@ const Navbar = () => {
         >
           <div>
             <div className="flex w-full items-center justify-between">
-              <Image
-                src="/../public/assets/logo.png"
+              {/* <Image
+                src="/../public/assets/logo.png" // TODO replace with logo
                 alt="logo"
                 width="100"
                 height="40"
-              />
+              /> */}
+              <p className="text-base uppercase">vignesh</p>
               <div
                 className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
                 onClick={handleNav}
@@ -72,7 +74,7 @@ const Navbar = () => {
             </div>
             <div className="border-b border-gray-300 my-4">
               {/* TODO */}
-              <p className="w-[85%] md:w-[90%] py-4">Building fun software!</p>
+              {/* <p className="w-[85%] md:w-[90%] py-4">Building fun software!</p> */}
             </div>
           </div>
           <div className="py-4 flex flex-col">
