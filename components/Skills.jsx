9e68203eafdc3dc87f0b738b2fcda4002f70f0b1
Vigneshraj Sekar Babu <vignesh.raj47@gmail.com>
2022-08-30 23:51:51 -0700

    feat: skills update, icons

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,7 +1,146 @@
+import Image from "next/image";
 import React from "react";
-
+// TODO get high res icons
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="w-full lg:h-screen p-2">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Skills
+        </p>
+        <h2 className="py-4">What I Can Do</h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/nodejs.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                ></Image>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>NodeJS</h3>
+                {/* <p className="text-sm text-gray-500 italic">(everyday)</p> */}
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/playwright.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                ></Image>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Playwright</h3>
+                {/* <p className="text-sm text-gray-500 italic">(everyday)</p> */}
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/nextjs.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                ></Image>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Next.js</h3>
+                {/* <p className="text-sm text-gray-500 italic">(everyday)</p> */}
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/aws.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                ></Image>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>AWS</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/github.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                ></Image>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Github</h3>
+                {/* <p className="text-sm text-gray-500">everyday</p> */}
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/datadog.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                ></Image>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Datadog</h3>
+                {/* <p className="text-sm text-gray-500">past</p> */}
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/graphql.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                ></Image>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>GraphQL</h3>
+                {/* <p className="text-sm text-gray-500">past</p> */}
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/selenium.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                ></Image>
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Selenium</h3>
+                {/* <p className="text-sm text-gray-500">past</p> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import About from "../components/About";
 import { Main } from "../components/Main";
 import Navbar from "../components/Navbar";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Navbar />
       <Main />
       <About />
+      <Skills />
     </div>
   );
 }
