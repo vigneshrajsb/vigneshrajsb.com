98459cc4a119feea11cf2abaf28952e34b3cd6a9
vigneshrajsb <vignesh.raj47@gmail.com>
2022-10-04 03:16:18 -0700

    fix skill comp in mobile

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -6,15 +6,12 @@ const Skill = ({ icon, image, name }) => {
   return (
     <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
-        <div className="m-auto invisible md:visible">
+        <div className="m-auto ">
           {icon && React.createElement(icon, { size: '50' })}
-          {image && (
-            <Image src={image} width="64px" height="64px" alt="/"></Image>
-          )}
+          {image && <Image src={image} width="50x" height="50px" alt="/" />}
         </div>
         <div className="flex flex-col items-center justify-center">
-          <h3>{name}</h3>
-          {/* <p className="text-sm text-gray-500 italic">(everyday)</p> */}
+          <h3 className="text-sm md:text-base">{name}</h3>
         </div>
       </div>
     </div>
