0e8cc28aa18add4f927f7f118f6f999439197bfe
Vigneshraj Sekar Babu <vignesh.raj47@gmail.com>
2022-10-06 15:55:36 -0700

    main comp chevron

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,27 +2,29 @@ import React from 'react'
 import { FaLinkedinIn, FaGithub, FaTwitter } from 'react-icons/fa'
 import { AiOutlineMail } from 'react-icons/ai'
 import Link from 'next/link'
+import { HiChevronDown } from 'react-icons/hi'
+
 export const Main = () => {
   return (
-    <div id="home" className="w-full h-screen text-center bg-[#222]">
+    <div id="home" className="w-full h-screen text-center bg-[#222] relative">
       <div className="max-w-[1240p] w-full h-full mx-auto p-2 flex justify-center items-center hover:cursor-default">
         <div>
-          <p className="uppercase text-sm tracking-widest text-[#d4d4d4]  hover:cursor-default">
+          <p className="uppercase text-sm tracking-widest text-[#d4d4d4] hover:cursor-default">
             LET&apos;S BUILD SOFTWARE TOGETHER!
           </p>
-          <h1 className="py-4 text-[#d4d4d4]  md:hover:scale-125 ease-in duration-500">
+          <h1 className="py-2 md:py-4 text-[#d4d4d4]  md:hover:scale-125 ease-in duration-500">
             Hi, I&apos;m <span className="text-[#e06e4d] "> Vignesh</span>
           </h1>
-          <h2 className="py-2 text-[#d4d4d4]">Software Engineer</h2>
-          <p className="py-1 text-[#d4d4d4] font-light">(he/him)</p>
-          <p className="py-4 text-[#d4d4d4] max-w-[70%] m-auto  font-light text-sm md:text-base">
+          <h2 className="py-1 md:py-2 text-[#d4d4d4]">Software Engineer</h2>
+          <p className="py-0 md:py-1 text-[#d4d4d4] font-light">(he/him)</p>
+          <p className="py-2 md:py-4 text-[#d4d4d4] max-w-[70%] m-auto  font-light text-sm md:text-base">
             I&apos;m a software developer specialized in building tools that
             help with developer productivity, internal tooling and testing
             practices. Currently, I&apos;m a part of the Developer Productivity
             team building tools and serverless applications while learning SRE
             technologies.
           </p>
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-2 md:py-4">
             <a
               href="https://www.linkedin.com/in/vigneshrajsb/"
               target={'_blank'}
@@ -57,6 +59,15 @@ export const Main = () => {
             </a>
           </div>
         </div>
+      </div>
+      <div className="absolute bottom-0 w-full flex items-center justify-center py-1 md:py-6 col-span-3">
+        <Link href="/#about">
+          <a>
+            <div className="rounded-full shadow-lg shadow-gray-900 p-3 md:p-4 cursor-pointer hover:scale-110 ease-in duration-300 hover:ring-1 hover:ring-white animate-pulse">
+              <HiChevronDown className="text-[#d4d4d4] text-xs md:text-lg" />
+            </div>
+          </a>
+        </Link>
       </div>
     </div>
   )
